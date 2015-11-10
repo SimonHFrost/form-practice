d552167f1a55fdd8587347b5e674350188acaaee
simonhfrost <simonhfrost@gmail.com>
2015-11-10 21:19:07 +0100

    Date format validation

--- javascript/prepare-date-validation.js
+++ javascript/prepare-date-validation.js
@@ -1,17 +1,13 @@
-function prepareDateValidation( options ) {
+var DATE_REGEX = "[0-9]{1,2}\/[0-1]?[0-9]\/[0-9]{4}"
 
+function prepareDateValidation( options ) {
   var dateElement = document.querySelector( options.query )
   var formElement = document.querySelector( options.form )
 
-  var dateRegex = "[0-9]{1,2}\/[0-1]?[0-9]\/[0-9]{4}"
-
   formElement.addEventListener( 'submit', function( event ) {
-    event.preventDefault()
-
-    if ( dateElement.value.match( dateRegex ) ) {
-      alert( 'date regex is right' )
-    } else {
-      alert( 'date regex is wrong' )
+    if ( !dateElement.value.match( DATE_REGEX ) ) {
+      event.preventDefault()
+      alert( "Place use a format like '01/01/2001'" )
     }
   })
 
