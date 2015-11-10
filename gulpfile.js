6c2f0197e662123112a0c1224f68425f5ee68ba2
simonhfrost <simonhfrost@gmail.com>
2015-11-10 19:19:51 +0100

    Finish bundling structure

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp')
+var sourcemaps = require('gulp-sourcemaps')
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer')
+var browserify = require('browserify')
+var watchify = require('watchify')
+
+function compile (watch) {
+  var bundler = watchify(
+    browserify('./javascript/main.js', {
+      debug: true
+    }))
+
+  function rebundle () {
+    bundler.bundle()
+      .on('error', function (err) {
+        console.error(err)
+        this.emit('end')
+      })
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist'))
+  }
+
+  if (watch) {
+    bundler.on('update', function (fileChanged) {
+      console.log(Date.now() + ': ' + fileChanged + ' changed')
+      rebundle()
+    })
+  }
+
+  rebundle()
+}
+
+gulp.task('bundle', function () { return compile(false) })
+gulp.task('watch', function () { return compile(true) })
+
+gulp.task('default', ['watch'])
